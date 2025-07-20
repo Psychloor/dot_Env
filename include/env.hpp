2469264cb3e0026835f331f420bfd4178ab7ad58
blomq <blomqvistkevin@hotmail.com>
2025-07-20 17:39:06 +0200

    Add numeric environment variable retrieval with native endian support

--- include/env.hpp
+++ include/env.hpp
@@ -27,8 +27,9 @@ namespace dot_env
          *
          * @param filename The name of the file to load environment variables
          * from. If not specified, defaults to ".env".
-         * @param override_system If provided, overrides the compile-time default.
-         *        If not provided, uses the value set by DOT_ENV_OVERRIDE_SYSTEM.
+         * @param override_system If provided, overrides the compile-time
+         * default. If not provided, uses the value set by
+         * DOT_ENV_OVERRIDE_SYSTEM.
          * @return Returns true if the file was successfully found and parsed,
          *         otherwise returns false.
          */
@@ -90,6 +91,25 @@ namespace dot_env
          */
         std::optional<T> get_be(const std::string_view& key);
 
+        template <class T>
+            requires std::is_arithmetic_v<T>
+        /**
+         * Retrieves a numeric value of a specified type from the environment
+         * variables if it exists. All in native-endian byte order
+         *
+         * This function attempts to fetch the value associated with the given
+         * key from the environment, parse it into a numeric type, and return
+         * it. If the key does not exist or the value cannot be parsed, the
+         * function returns std::nullopt.
+         *
+         * @param key The key to search for in the environment variables.
+         * @tparam T The numeric type to which the environment variable
+         * should be converted. Must satisfy "std::is_arithmetic".
+         * @return An optional containing the parsed numeric value if
+         * successful; otherwise std::nullopt.
+         */
+        std::optional<T> get_ne(const std::string_view& key);
+
         std::string require(std::string_view key);
 
 
@@ -103,6 +123,21 @@ namespace dot_env
 
     template <typename T>
         requires std::is_arithmetic_v<T>
+    /**
+     * Retrieves an environment variable, parses it as an arithmetic type, and
+     * ensures the value is in little-endian format.
+     *
+     * This function fetches the value of the environment variable associated
+     * with the provided key, attempts to convert it to the specified arithmetic
+     * type, and ensures the value is returned in little-endian byte order. If
+     * the conversion fails or the key does not exist, the function returns an
+     * empty optional.
+     *
+     * @param key The key corresponding to the desired environment variable.
+     * @tparam T An arithmetic type to which the value will be converted.
+     * @return An optional containing the parsed value in little-endian format
+     * if successful, otherwise an empty optional.
+     */
     std::optional<T> env::get_le(const std::string_view& key)
     {
         const auto val = get(key);
@@ -129,6 +164,21 @@ namespace dot_env
 
     template <typename T>
         requires std::is_arithmetic_v<T>
+    /**
+     * Retrieves an environment variable, parses it as an arithmetic type, and
+     * ensures the value is in big-endian format.
+     *
+     * This function fetches the value of the environment variable associated
+     * with the provided key, attempts to convert it to the specified arithmetic
+     * type, and ensures the value is returned in big-endian byte order. If the
+     * conversion fails, or if the key does not exist, the function returns an
+     * empty optional.
+     *
+     * @param key The key corresponding to the desired environment variable.
+     * @tparam T An arithmetic type to which the value will be converted.
+     * @return An optional containing the parsed value in big-endian format
+     * if successful, otherwise an empty optional.
+     */
     std::optional<T> env::get_be(const std::string_view& key)
     {
         const auto val = get(key);
@@ -151,6 +201,39 @@ namespace dot_env
             // ReSharper disable once CppDFAUnreachableCode
             return std::byteswap(value);
         }
+    }
+
+
+    template <class T>
+        requires std::is_arithmetic_v<T>
+    /**
+     * Retrieves an environment variable and converts it to a specified
+     * arithmetic type.
+     *
+     * This function fetches the value of the environment variable associated
+     * with the provided key, attempts to convert it to the specified arithmetic
+     * type, and returns the parsed value if successful. If the variable is not
+     * found or the conversion fails, it returns an empty optional.
+     *
+     * @param key The key of the environment variable to retrieve and parse.
+     * @tparam T An arithmetic type to which the environment variable's value
+     * will be converted.
+     * @return An optional containing the parsed value if successful,
+     * otherwise an empty optional.
+     */
+    std::optional<T> env::get_ne(const std::string_view& key)
+    {
+        const auto val = get(key);
+        if (!val.has_value())
+            return std::nullopt;
+
+        T value;
+        auto [ptr, ec] =
+            std::from_chars(val->data(), val->data() + val->size(), value);
+        if (ec != std::errc{})
+            return std::nullopt;
+
+        return value;
     }
 
 } // namespace dot_env
